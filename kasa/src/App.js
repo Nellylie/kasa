2f8d9d90fe7bf41e782d1c5fdcecc000fcfe8740
Nellylie <marie.beka01@gmail.com>
2023-08-17 22:05:27 +0200

    feat(): recuperation données json

--- kasa/src/App.js
+++ kasa/src/App.js
@@ -1,10 +1,17 @@
 import "./sass/main.scss";
+import InitRouter from "./components/InitRouter/InitRouter";
+import { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-     
-    </div>
-  );
+  const urlJson = process.env.PUBLIC_URL + '/data/logements.json';
+  const [locations, setLocations] = useState([]);
+  useEffect(() => {
+    fetch(urlJson).then(
+      function (response) {
+        return response.json();
+      }).then(function (urlJson) { return setLocations(urlJson); });
+  }, []);
+
+  return <><InitRouter locations = {locations}/></>;
 }
 
 export default App;
