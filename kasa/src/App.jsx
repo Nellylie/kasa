a84683d2788bd4e4bd4a70695dadd8cc1bfe7a1c
Nellylie <marie.beka01@gmail.com>
2023-08-30 10:02:40 +0200

    test

--- kasa/src/App.jsx
+++ kasa/src/App.jsx
@@ -2,17 +2,16 @@ import './sass/main.scss';
 import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home/Home';
-import LocationSelected from './pages/locationselected/LocationSelected';
 import Error from './pages/error/Error';
 import About from './pages/about/About';
 import Header from './components/header/Header';
-
+import axios from 'axios';
 function App () {
   const [locations, setLocations] = useState([]);
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetch(process.env.PUBLIC_URL + 'data/logements.json').then((res) => { if (res.ok) { return res.json(); } throw res; }).then((set) => {
+    axios.get('/data/logements.json').then((res) => { if (res.ok) { return res.json(); } throw res; }).then((set) => {
       setLocations(set);
     }).catch((err) => {
       console.log(err); setError(true);
@@ -22,12 +21,12 @@ function App () {
 
   return <>
   <Header/>
+
  <Routes>
           <Route path="/" element= {<Home locations = {locations}/>}/>
           <Route path="/about" element= {<About/>}/>
           <Route path="/error" element= {<Error/>}/>
           <Route path="/*" element= {<Error/>}/>
-          <Route path="/home/:id" element={<LocationSelected />} />
         </Routes>;
   </>;
 }
--- kasa/src/pages/home/Home.jsx
+++ kasa/src/pages/home/Home.jsx
@@ -2,12 +2,17 @@ import LocationCards from '../../components/locationcards/LocationCards';
 import Footer from '../../components/footer/Footer';
 import Banner from '../../components/banner/Banner';
 import imgBannerHome from '../../media/image-banner-home.jpg';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 function Home ({ locations }) {
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('data/logements.json').then((res) => setData(res.data)); // requète AXIOS ici également pour prochaine utilisation API
+  }, []);
   return <>
   <Banner imgSrc={imgBannerHome}/>
   <div className='location-list'>
-  {locations.map((oneLocation) => (<LocationCards className = "card-container"
+  {data.map((oneLocation) => (<LocationCards className = "card-container"
                 key = {oneLocation.id} id = {oneLocation.id} title = {oneLocation.title} cover = {oneLocation.cover}
             />))}
   </div>
