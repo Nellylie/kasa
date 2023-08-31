7586a90a712d6a5946952e44fd52f41a31e8adfb
Nellylie <marie.beka01@gmail.com>
2023-08-31 10:23:01 +0200

    feat() page erreur 404

--- kasa/src/pages/error/Error.jsx
+++ kasa/src/pages/error/Error.jsx
@@ -1,5 +1,19 @@
+import { Link } from 'react-router-dom';
+
 function Error () {
-  return <div>Erreur</div>;
+  const error = "oups ! La page que vous demandez n'existe pas";
+  const back = "Retourner sur la page d'accueil";
+
+  return (
+    <div className="error-section">
+        <h1>404</h1>
+        <h2>{error}</h2>
+        <div className="linkBack">
+            <Link to= {'/' } className="error-back">{back}
+            </Link>
+        </div>
+    </div>
+  );
 }
 
 export default Error;
